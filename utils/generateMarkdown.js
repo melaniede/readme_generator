bf19113e070534d542b3f91739d5a3f0c06f590c
melaniede <debarrosmelanie@gmail.com>
2020-11-07 20:09:51 -0500

    add license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,23 @@
+const licenseBadgeList = {
+  "MIT License": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  "GNU GPLv3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  "Mozilla Public License 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+  "The Unlicense":"[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+}
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  data.licenseBadge = licenseBadgeList[data.license];
+  return `
+  # ${data.title}
+  ${data.licenseBadge}
   ## Description
   ${data.description}  
   ## Table of Contents
   * [Installation](#installation)
   * [Usage](#usage)
-  * [Contributing](#contribution)
+  * [Contribution](#contribution)
   * [Tests](#tests)
   * [License](#license)
   * [Questions](#questions)
